b81401a2651b8928651d8cfe262a572ddea16701
seanlongcc <seanlongcc@gmail.com>
2024-11-21 01:07:16 -0500

    update 11

---
+++ Two Pointers/11.container-with-most-water.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=11 lang=python3
+#
+# [11] Container With Most Water
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        pass
+
+# @lc code=end
+
+
+Solution().maxArea(height=[1, 8, 6, 2, 5, 4, 8, 3, 7])
